a0adbfd6c0e0d1bcff4ed16edb3394ffd5ab2b5e
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-06-25 17:36:09 +0300

    update deploy

--- src/components/Api/JsonthemoviedbApi.js
+++ src/components/Api/JsonthemoviedbApi.js
@@ -9,12 +9,12 @@ export const getMovies = async () => {
   return data;
 };
 
-const searchMovies = async query => {
-  const { data } = await axios.get(
-    `https://api.themoviedb.org/3/search/movie?api_key=${myApiKey}&query=${query}`
-  );
-  return data;
-};
+// export const searchMovies = async query => {
+//   const { data } = await axios.get(
+//     `https://api.themoviedb.org/3/search/movie?api_key=${myApiKey}&query=${query}`
+//   );
+//   return data;
+// };
 
 export const detailsMovies = async id => {
   console.log(id);
@@ -31,35 +31,14 @@ export const castMovies = async id => {
   return data;
 };
 
-const reviewsMovies = async query => {
-  const { data } = await axios.get(
-    `https://api.themoviedb.org/3/movie/movie_id/reviews?api_key=${myApiKey}`
-  );
-  return data;
-};
+// export const reviewsMovies = async query => {
+//   const { data } = await axios.get(
+//     `https://api.themoviedb.org/3/movie/movie_id/reviews?api_key=${myApiKey}`
+//   );
+//   return data;
+// };
 
 // 'https://api.themoviedb.org/3/movie/movie_id',
 // 'https://api.themoviedb.org/3/movie/movie_id/credits';
 // 'https://api.themoviedb.org/3/movie/movie_id/reviews';
 //api.themoviedb.org/3/search/movie?api_key=###&query=transformers
-// const options = {
-//   method: 'GET',
-//   url: 'https://api.themoviedb.org/3/authentication',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer e4e77b2d83733ec5f4ee2698cbe57afc',
-//   },
-// };
-
-// axios
-//   .request(options)
-//   .then(function (response) {
-//     console.log(response.data);
-//   })
-//   .catch(function (error) {
-//     console.error(error);
-//   });
-
-// const myApiKey = 'e4e77b2d83733ec5f4ee2698cbe57afc';
-
-// export default getMovies, detailsMovies;
